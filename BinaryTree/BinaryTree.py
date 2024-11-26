65fad072ef678b090e4ae63d4ae63c5878e7f6a5
Dowon Lee <ldwlee03@gmail.com>
2024-11-26 09:35:10 +0900

    ㅇ

---
+++ BinaryTree/BinaryTree.py
@@ -0,0 +1,12 @@
+class BinaryTree :    
+    class Node :
+        def __init__(self, item) :
+            self.data = item
+            self.left = None
+            self.right = None
+
+        def size(self) :
+            l = self.left.size() 
+    
+    def __init__(self, r) :
+        self.root = r
